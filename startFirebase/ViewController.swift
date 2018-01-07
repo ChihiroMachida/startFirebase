0e7134a2208f63d831b5d50da48a3c2fcccb46e3
chihiro <16201chihiro@gmail.com>
2018-01-07 10:56:32 +0900

    Initial Commit

---
+++ startFirebase/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  startFirebase
+//
+//  Created by 町田千優 on 2018/01/07.
+//  Copyright © 2018年 町田千優. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
